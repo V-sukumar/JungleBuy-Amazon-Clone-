45df550d5988689d8f350a46d260fde1834cf6cf
V-sukumar <115460777+V-sukumar@users.noreply.github.com>
2023-02-23 11:54:28 +0530

    Update products.js

--- Project amazon/products.js
+++ Project amazon/products.js
@@ -1,51 +1,92 @@
-let apiUrl = "https://63c812db075b3f3a91d99323.mockapi.io/Clothes";
+let productsContainer = document.querySelector(".products-container");
+let prevBtn = document.querySelector(".previous-page");
+let nextBtn = document.querySelector(".next-page");
+let pageOneBtn = document.querySelector(".one");
+let pageTwoBtn = document.querySelector(".two");
 
-let resultsContainer = document.querySelector(".results");
+let currentPage = 1;
+let productsPerPage = 12;
+let totalPages = 2;
+let productsData = [];
 
-fetch(apiUrl)
+fetch("https://63c812db075b3f3a91d99323.mockapi.io/Clothes")
   .then((response) => response.json())
-  .then((products) => {
-    let cards = products.map((product) => {
-      let card = document.createElement("div");
-      card.classList.add("card");
-
-      let image = document.createElement("img");
-      image.src = product.Image;
-      image.alt = product.title;
-      card.appendChild(image);
-
-      let title = document.createElement("h3");
-      title.textContent = product.Title;
-      card.appendChild(title);
-
-      let description = document.createElement("p");
-      description.textContent = product.Description;
-      card.appendChild(description);
-
-      let rating = document.createElement("span");
-      rating.classList.add("rating");
-      rating.textContent = product.Rating;
-      card.appendChild(rating);
-
-      let price = document.createElement("span");
-      price.classList.add("price");
-      price.textContent = product.Price + "rs";
-      card.appendChild(price);
-
-      return card;
-    });
-
-    for (let i = 0; i < cards.length; i += 4) {
-      let row = document.createElement("div");
-      row.classList.add("row");
-
-      for (let j = i; j < i + 4 && j < cards.length; j++) {
-        row.appendChild(cards[j]);
-      }
-
-      resultsContainer.appendChild(row);
-    }
+  .then((data) => {
+    productsData = data;
+    renderPage(currentPage);
   })
-  .catch((error) => {
-    console.error("Error fetching products:", error);
+  .catch((error) => console.error(error));
+
+function renderPage(page) {
+  productsContainer.innerHTML = "";
+
+  let startIndex = (page - 1) * productsPerPage;
+  let endIndex = startIndex + productsPerPage;
+
+  let currentProducts = productsData.slice(startIndex, endIndex);
+
+  currentProducts.forEach((product) => {
+    let card = document.createElement("div");
+    card.classList.add("product-card");
+
+    let image = document.createElement("img");
+    image.src = product.Image;
+    image.alt = product.name;
+    card.appendChild(image);
+
+    let name = document.createElement("h3");
+    name.textContent = product.Title;
+    card.appendChild(name);
+
+    let description = document.createElement("p");
+    description.textContent = product.Description;
+    card.appendChild(description);
+
+    let rating = document.createElement("div");
+    rating.classList.add("rating");
+    rating.textContent = product.Rating;
+
+    for (let i = 0; i < 5; i++) {
+      let icon = document.createElement("img");
+      icon.src = "icons8.png";
+      rating.appendChild(icon);
+    }
+
+    card.appendChild(rating);
+
+    let price = document.createElement("p");
+    price.textContent = `₹${product.Price}` + `(20% off)`;
+    card.appendChild(price);
+
+    productsContainer.appendChild(card);
   });
+
+  currentPage = page;
+  if (currentPage == 1) {
+    prevBtn.disabled = true;
+    pageOneBtn.disabled = true;
+  } else {
+    prevBtn.disabled = false;
+    pageOneBtn.disabled = false;
+  }
+  if (currentPage == totalPages) {
+    nextBtn.disabled = true;
+    pageTwoBtn.disabled = true;
+  } else {
+    nextBtn.disabled = false;
+    pageTwoBtn.disabled = false;
+  }
+}
+
+prevBtn.addEventListener("click", () => {
+  renderPage(currentPage - 1);
+});
+nextBtn.addEventListener("click", () => {
+  renderPage(currentPage + 1);
+});
+pageOneBtn.addEventListener("click", () => {
+  renderPage(1);
+});
+pageTwoBtn.addEventListener("click", () => {
+  renderPage(2);
+});
